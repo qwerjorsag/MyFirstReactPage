ee391c37a33e8b2d73556af0e0e9c24464e25943
qwerjorsag <qwerjorsag@gmail.com>
2024-12-03 23:10:00 +0100

    Zha

--- src/Pages/NewsPage.tsx
+++ src/Pages/NewsPage.tsx
@@ -30,7 +30,7 @@ const NewsPage: React.FC = () => {
 
     return (
         <div className="news-page">
-            <h1>novinky</h1>
+            <h1>Žha novinky</h1>
             <div className="news-list">
                 {currentNews.map((news) => (
                     <NewsItem
