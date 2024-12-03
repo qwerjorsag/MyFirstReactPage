d8968209446a74c04b4597d831a28c6f7b08a928
qwerjorsag <qwerjorsag@gmail.com>
2024-12-03 22:03:21 +0100

    Zhave novinky commit uz jen novinky

--- src/Pages/NewsPage.tsx
+++ src/Pages/NewsPage.tsx
@@ -30,7 +30,7 @@ const NewsPage: React.FC = () => {
 
     return (
         <div className="news-page">
-            <h1>Žhavé novinky</h1>
+            <h1>novinky</h1>
             <div className="news-list">
                 {currentNews.map((news) => (
                     <NewsItem
